c42b88a34160546d62b3de4b2b893d4d686a0864
xlincw0w <khaled.khazem@yahoo.fr>
2019-02-24 22:03:47 +0100

    Updated

--- src/Components/Submit/Submit.js
+++ src/Components/Submit/Submit.js
@@ -5,7 +5,7 @@ const Submit = () => {
     return (
     <div className="w-100">
         <a href="" className="Submit db mt3 center f4 shadow-5 no-underline white hover-light-red pa3 ba">
-            <span className="md2">==> Recherchez</span>
+            <span className="md2"> => Recherchez</span>
         </a>
     </div>
     );
