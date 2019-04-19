1718fd46edc6f7564bbf3ed244a12b1aefaeb0bd
xlincw0w <khaled.khazem@yahoo.fr>
2019-04-19 19:42:32 +0100

    Design updated

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,7 +4,7 @@ import './Contact.css';
 const Contact = ( {onPageChange} ) => {
     return (
     <div className="contact mt5 w-75 h-50 db center tc ba b--black">
-        <h1 className="light-red db mb5 pt5">Nous Contacter :</h1>
+        <h1 className="light-red db mb5 pt5 f1">☟</h1>
         <div className="white">
         <h2>Plateform web developper pour un project mémoire  !</h2>
         <h4>Groupe : </h4>
@@ -12,7 +12,7 @@ const Contact = ( {onPageChange} ) => {
         <p>Dehbia Djebarri</p>
         <p>Said Zidelmal</p>
         <p>Amel Hameg</p>
-        <h2 className="pt4 light-red">Aide et contact :</h2>
+        <h2 className="pt4 light-red">◄ Aide et contact ►</h2>
         <h2 className="mb5">Vous cherchez à joindre le service client ?
             Vous avez besoin d'aide ou de conseils ?</h2>
             <h3>Tel : +21300000000</h3>
